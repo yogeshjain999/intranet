b653e0da7a51ae44bd0d42763526b1b2dfe981de
Shailesh Patil <shailesh@joshsoftware.com>
2012-10-30 12:53:00 +0530

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JoshIntranet::Application.config.secret_token = '761941675c0a661f7c69a8f1d3d5062f81eb706275e65873541adf51b2c599378d8e0bab330e0e5da313ae523dc069c081bbe52d6fb754cc8aed3dc748169ed8'
