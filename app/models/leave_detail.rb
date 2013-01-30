ff13cedd63e39d76d6b9cdb8fce174a308288245
Nitin Dhaware <nitin@techvision.net.in>
2013-01-30 12:46:54 +0530

    Changing leaveDetails

--- app/models/leave_detail.rb
+++ app/models/leave_detail.rb
@@ -1,7 +1,7 @@
 class LeaveDetail
   include Mongoid::Document
   embedded_in :user
-
-  field :leave_types, type: Hash
+  field :assign_date, type: Date
+  field :assign_leaves, type: Hash
   field :available_leaves, type: Hash
 end
