a1cf72cd6b39cc655b1fb52750d7e2b3001572da
Nitin Dhaware <nitin@techvision.net.in>
2013-02-14 18:02:40 +0530

    Added validations for number of leaves and fixed date validations

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -24,7 +24,14 @@ class LeavesController < ApplicationController
   end
 
   def create
+    available_leaves = nil
+    current_user.leave_details.each do |l|
+      if l.assign_date.year == Date.today.year
+        available_leaves = l.available_leaves
+      end
+        end
     @leave = Leave.new(params[:leave])
+    @leave.access_params(params[:leave], available_leaves)
     @leave.user = current_user
     user = User.find(current_user)
     @leave.organization = current_organization
@@ -49,12 +56,13 @@ class LeavesController < ApplicationController
 	format.html {redirect_to @leave, notice: 'Your request has been noted' }
         format.json {render json: @leave, status: :created}
       else
+        @profile = current_user.profile
         format.html {render action: "new"}
 	format.json { render json: @leave.errors, status: :unprocessable_entity }
       end
     end
   end
-#end
+
   def edit
     @leave = Leave.find(params[:id])
   end
