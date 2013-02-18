fba5bbdf6731470eb743ff3b914dc3c5d33c1afd
vishal pawle <vishal@techvision.net.in>
2013-02-18 11:03:08 +0530

    nothing

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -2,12 +2,14 @@ class LeavesController < ApplicationController
 
   def index        
     @leaves = current_organization.leaves.accessible_by(current_ability)
+
+
     current_user.leave_details.each do |l|
       if l.assign_date.year == Date.today.year
         @leave_details = l
     end
     end
-    @leave_types = current_organization.leave_types.all
+    @leave_types = current_organization.leave_types
     respond_to do |format|
       format.html # index.html.haml
       format.json { render json: @leaves }
