1bbc9f97f5fcfb54f6f34e0496a48bd528dae94a
vishal pawle <vishal@techvision.net.in>
2013-02-20 15:40:57 +0530

    show page has been created

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -13,7 +13,7 @@ class Profile
   field :phone_number, type: Integer
   field :dateOfBirth, type: Date
   field :passport_number, type: String
-  validates :name, :local_address, :permanent_address,    :dateOfBirth, :pan_number, :phone_number, :presence => true
+  validates :name, :local_address, :permanent_address,    :dateOfBirth, :phone_number, :presence => true
     validates :phone_number, :length => { :is => 10 }, :allow_blank => true 
   validates :pan_number, :length => { :is => 10 }, :allow_blank => true 
   validates :pan_number, :format => { :with => /\A[A-Z]{5}\d{4}[A-Z]{1}\Z/ , :message => 'invalid pan number'}, :allow_blank => true 
