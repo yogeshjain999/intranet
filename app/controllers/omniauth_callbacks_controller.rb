e6c4e22019d551db27aef74df3f41e3daa5d5e44
pramodshinde <pramod@joshsoftware.com>
2013-09-24 13:52:43 +0530

    google log in validation updated, datepicker javascript fixed

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,15 +1,18 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     user = User.from_omniauth(request.env["omniauth.auth"])
-    p "============#{request.env["omniauth.auth"]}"
-    p "---------#{user.inspect}-----#{user.persisted?}=====#{user.errors.full_messages}"
-    if user.persisted?
-      flash.notice = "Signed in Through Google!"
-      sign_in_and_redirect user
+    if user
+      if user.persisted?
+        flash.notice = "Signed in Through Google!"
+        sign_in_and_redirect user
+      else
+        session["devise.user_attributes"] = user.attributes
+        flash.notice = "You are almost Done! Please provide a password to finish setting up your account"
+        redirect_to new_user_registration_url
+      end
     else
-      session["devise.user_attributes"] = user.attributes
-      flash.notice = "You are almost Done! Please provide a password to finish setting up your account"
-      redirect_to new_user_registration_url
+      flash.notice = "You are not authorized to sign in!"
+      redirect_to new_user_session_path
     end
   end
 end
