bcf3184d3ab389dc906d686ad0ddfbb7f313c2a0
pramodshinde <pramod@joshsoftware.com>
2013-09-24 13:52:43 +0530

    google log in validation updated, datepicker javascript fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require bootstrap-datepicker
 //= require turbolinks
+//= require bootstrap-datepicker
 //= require_tree .
