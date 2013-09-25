a73bb9bff40904095f84cca8cbdc6e78c33a8ed5
Priyanka <priyanka@joshsoftware.com>
2013-09-25 19:09:44 +0530

    Test cases for Profile related models.

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Address do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it { should have_fields(:type_of_address, :flat_or_house_no, :building_or_society_name, :road, :locality, :city, :state, :phone_no, :same_as_permanent_address) }
+  it { should have_field(:same_as_permanent_address).of_type(Boolean).with_default_value_of(false) }
+  it { should be_embedded_in :private_profile }
+
 end
--- spec/models/private_profile_spec.rb
+++ spec/models/private_profile_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe PrivateProfile do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it { should have_fields(:pan_number, :personal_emailid, :passport_number, :qualification, :date_of_joining, :date_of_relieving, :work_experience, :previous_company) }
+  it { should have_field(:date_of_joining).of_type(Date) }
+  it { should have_field(:date_of_relieving).of_type(Date) }
+  it { should embed_many :addresses }
+  it { should embed_many :contact_persons }
+  it { should be_embedded_in(:user) }
+  it { should accept_nested_attributes_for(:addresses) }
+  it { should accept_nested_attributes_for(:contact_persons) }
+  it { should validate_presence_of(:qualification).on(:update) }
+  it { should validate_presence_of(:date_of_joining).on(:update) }
+  it { should validate_presence_of(:personal_emailid).on(:update) }
 end
+  
