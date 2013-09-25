65a28497ea6e231cbe5c9f87bdd0fdd291f688f3
pramodshinde <pramod@joshsoftware.com>
2013-09-25 16:29:43 +0530

    user invitation updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,12 +24,15 @@ class UsersController < ApplicationController
   def invite_user
     if request.get?
       @user = User.new
-      @user.build_public_profile
-      @user.build_private_profile
     else
       @user = User.new(params[:user].permit(:email, :role))
       @user.password = Devise.friendly_token[0,20]
-      if @user.save
+      @user.build_public_profile
+      @user.build_private_profile
+      2.times {@user.private_profile.relative_details.build}
+      ADDRESSES.each{|a| @user.private_profile.addresses.build({:type_of_address => a})}
+
+      if @user.save!
         flash.notice = 'Invitation sent Succesfully'
         UserMailer.delay.invitation(current_user, @user)
         redirect_to root_path
