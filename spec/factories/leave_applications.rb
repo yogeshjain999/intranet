175b2697a71ab28c5577a0420ab5321771f6f9d7
sanjiv <sanjiv@joshsoftware.com>
2013-10-15 17:44:15 +0530

    added the validator spec for leave_application #5

--- spec/factories/leave_applications.rb
+++ spec/factories/leave_applications.rb
@@ -2,8 +2,8 @@
 
 FactoryGirl.define do
   factory :leave_application do |l|
-    starts_at Date.today + 2
-    ends_at Date.today + 3
+    start_at Date.today + 2
+    end_at Date.today + 3
     reason "Sick"
     contact_number "1234567890"
     l.association(:address)
--- spec/models/leave_application_spec.rb
+++ spec/models/leave_application_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe LeaveApplication do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_fields(:start_at, :end_at, :reason, :contact_number, :reject_reason, :leave_status) }
+  it { should belong_to(:user) }
+  it { should belong_to(:leave_type) }
+  it { should validate_presence_of(:start_at) }
+  it { should validate_presence_of(:end_at) }
+  it { should validate_presence_of(:reason) }
+  it { should validate_presence_of(:contact_number) }
+  
 end
