e7d35989daeb8eed74313f1acd8e7ddb117d2de0
pramodshinde <pramod@joshsoftware.com>
2013-10-18 14:41:35 +0530

    admin route added, layout changed, document type valdation added

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user
       if user.persisted?
-        flash.notice = "Signed in Through Google!"
+        flash.notice = "Signed in Successfully!"
         sign_in_and_redirect user
       else
         session["devise.user_attributes"] = user.attributes
