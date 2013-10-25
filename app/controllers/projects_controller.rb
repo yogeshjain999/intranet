6123c729def92a148cb9388edac1ca7dd26262d5
pramodshinde <pramod@joshsoftware.com>
2013-10-25 22:07:47 +0530

    authenticated user for project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
-  load_and_authorize_resource
+  #load_and_authorize_resource 
+  before_action :authenticate_user!
   before_action :load_project, except: [:index, :new, :create]
 
   def index
