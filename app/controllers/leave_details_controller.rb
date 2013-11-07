db41709d2e0646a746fab52947ec50785c069862
Sanjiv Kumar Jha <sanjiv@joshsoftware.com>
2013-11-07 22:00:00 +0530

    added the inline editing available leave #5

--- app/controllers/leave_details_controller.rb
+++ app/controllers/leave_details_controller.rb
@@ -2,6 +2,18 @@ class LeaveDetailsController < ApplicationController
   before_action :authenticate_user!
 
   def update_available_leave
-
+    leave_details = LeaveDetail.find(params[:id])
+    leave_details.available_leave[leave_type] = params[:value]
+    leave_details.save 
+    render nothing: true
   end 
+
+  private
+    def leave_type
+      if ["Sick", "Casual", "TotalPaid"].include?(params[:type])
+        return params[:type]
+      else
+        raise
+      end
+    end
 end
