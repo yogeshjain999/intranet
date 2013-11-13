c541cbd5507c9070089f873bd54d0acc419788b5
Sanjiv Kumar Jha <sanjiv@joshsoftware.com>
2013-11-13 20:53:38 +0530

    added history tracker, global constants

---
+++ config/initializers/global_constants.rb
@@ -0,0 +1,5 @@
+GENDER = ['Male', 'Female']
+ADDRESSES = ['Permanent Address', 'Temporary Address']
+BLOOD_GROUPS = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
+STATUS = ['created', 'pending', 'approved']
+
