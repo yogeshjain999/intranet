7149d84a5126fe214bd4b4d1b00f691610cb754b
Sanjiv Kumar Jha <sanjiv@joshsoftware.com>
2013-11-13 15:50:57 +0530

    added the neg spec for user model #5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,4 +27,13 @@ describe User do
     user.leave_details.first.available_leave["CurrentPrivilege"].to_f.should be(1.5)
     user.leave_details.first.available_leave["TotalPrivilege"].to_f.should be(1.5)
   end
+
+  it "should not increment user privilege leave monthly if previous month days > 15" do
+    user = FactoryGirl.build(:user, private_profile: FactoryGirl.build(:private_profile, date_of_joining: Date.new(Date.today.year, Date.today.month - 1, 19)))
+    user.save!
+    user.assign_monthly_leave
+    user = User.last 
+    user.leave_details.first.available_leave["CurrentPrivilege"].to_f.should be(0.0)
+    user.leave_details.first.available_leave["TotalPrivilege"].to_f.should be(0.0)
+  end
 end
