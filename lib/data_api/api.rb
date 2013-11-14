f14e0b7ff8138118c5910faba526352ca26399a7
Sethupathi Asokan <sethu@joshsoftware.com>
2013-11-14 20:15:44 +0530

    Modified bonusly display format

--- lib/data_api/api.rb
+++ lib/data_api/api.rb
@@ -36,7 +36,20 @@ module Api
     end
 
     def bonusly_message_for(m, options = {})
-      "#{m["receiver"]["name"]} got #{m["amount"]} bonus from #{m["giver"]["name"]} for #{m["reason"]} in #{m["value"]}"
+      "<li class='media'>
+        <a class='pull-left' href='#'>
+          <img class='media-object' src='#{m["receiver"]["profile_pic_url"]}' alt='#{m["receiver"]["name"]}' /> 
+        </a>
+        <a class='pull-right' href='#'>
+          <img class='media-object' src='#{m["giver"]["profile_pic_url"]}' alt='#{m["giver"]["name"]}' /> 
+        </a>
+        <div class='media-body'>
+          <h5 class='media-heading'>#{m["receiver"]["name"]} received <span class='label label-info'>₹#{m["amount"]}</span> from #{m["giver"]["name"]} <span class='label label-warning'> #{m["value"]} </span></h5>
+          <p>
+            #{m["reason"]} 
+          </p>
+        </div>
+      </li>"
     end
 
     {"created_at"=>"2013-10-29T15:13:00Z", "reason"=>"helping out...", "amount"=>300, "value"=>"problem solving", "giver"=>{"name"=>"aditya shedge", "email"=>"aditya@joshsoftware.com", "path"=>"/company/users/526e500c9b6dab1c8d000001", "full_pic_url"=>"/assets/noPic_80.png", "profile_pic_url"=>"/assets/noPic_80.png", "small_pic_url"=>"/assets/noPic_80.png", "picture_url"=>"/assets/noPic_80.png", "first_name"=>"aditya", "last_name"=>"shedge"}, "receiver"=>{"name"=>"Shailesh", "email"=>"shailesh@joshsoftware.com", "path"=>"/company/users/5255067e2d88acd7be000001", "full_pic_url"=>"/assets/noPic_80.png", "profile_pic_url"=>"/assets/noPic_80.png", "small_pic_url"=>"/assets/noPic_80.png", "picture_url"=>"/assets/noPic_80.png", "first_name"=>nil, "last_name"=>nil}}
