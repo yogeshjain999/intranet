d85cbd2f4d06c4233f94ba0b8e6e7f92947155cc
anuja <anuja@joshsoftware.com>
2014-01-06 14:50:32 +0530

    Bug fixed in leave details + Updated specs
    @Sanjiv: Please review changes.

--- app/models/leave_detail.rb
+++ app/models/leave_detail.rb
@@ -4,27 +4,30 @@ class LeaveDetail
   include Mongoid::History::Trackable
 
   belongs_to :user
-  
+
   field :year,            type: Integer
-  
+
   field :available_leave,     type: Hash, default: {"Sick" => 0, "Casual" => 0, "CurrentPrivilege" => 0, "TotalPrivilege" => 0}
+
+  scope :this_year, where(year: Date.today.year)
+
   track_history  
- 
+
   before_save do
     self.available_leave["Sick"] = self.available_leave["Sick"].to_i
     self.available_leave["Casual"] = self.available_leave["Casual"].to_i
     self.available_leave["CurrentPrivilege"] = self.available_leave["CurrentPrivilege"].to_d.to_s
     self.available_leave["TotalPrivilege"] = self.available_leave["TotalPrivilege"].to_d.to_s
   end
-  
+
   def validate_doj_month
-    (self.user.private_profile.date_of_joining.month != (Date.today.month-1))
+    (self.user.private_profile.date_of_joining.month != Date.today.prev_month.month)
   end
-  
+
   def validate_doj_days
-    ((self.user.private_profile.date_of_joining.month == (Date.today.month-1)) && (self.user.private_profile.date_of_joining.day <= 15))
+    ((self.user.private_profile.date_of_joining.month == Date.today.prev_month.month) && (self.user.private_profile.date_of_joining.day <= 15))
   end 
-   
+
   def monthly_paid_leave
     if validate_doj_month || validate_doj_days
       self.available_leave["CurrentPrivilege"] = (self.available_leave["CurrentPrivilege"].to_d + 1.5).to_s
@@ -32,14 +35,14 @@ class LeaveDetail
     end
     self 
   end
-  
+
   def validate_leave(name, number_of_day)
     if name == "Privilege"
       name = "TotalPrivilege"
     end
     ((self.available_leave[name].to_d - number_of_day).to_d >= 0).blank? ? true: false          
   end
-  
+
   def add_rejected_leave(leave_type: "Sick", no_of_leave: 1)
     if leave_type != "Privilege"
       self.available_leave[leave_type] = self.available_leave[leave_type] + no_of_leave.to_i 
@@ -49,7 +52,7 @@ class LeaveDetail
     end
     self.save
   end 
-   
+
   def deduct_available_leave(leave_type: "Sick", no_of_leave: 1)
     if leave_type != "Privilege"
       self.available_leave[leave_type] = self.available_leave[leave_type] - no_of_leave
--- spec/factories/leave_applications.rb
+++ spec/factories/leave_applications.rb
@@ -9,4 +9,21 @@ FactoryGirl.define do
     reason "Sick"
     contact_number "1234567890"
   end
+  factory :casual_leave_application, class: LeaveApplication do |l|
+    association :leave_type, factory: :casual_type
+    start_at Date.today + 2
+    end_at Date.today + 3
+    number_of_days 2
+    reason "casual leave application"
+    contact_number "1234567890"
+  end
+  
+  factory :privilege_leave_application, class: LeaveApplication do |l|
+    association :leave_type, factory: :privilege_type
+    start_at Date.today + 2
+    end_at Date.today + 3
+    number_of_days 2
+    reason "privilege leave application"
+    contact_number "1234567890"
+  end
 end
