b76b6889632f300e61ff41b626714a4534f81455
Gautam Rege <gautam@joshsoftware.com>
2014-07-30 14:08:44 +0530

    refs #41 Upgraded to Rails 4.1.1 & some cleanup
    Upgraded to Rails 4.1.1
    Added CodeClimate, CircleCI and TestCoverage bades
    Update README

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,16 +11,10 @@ class Project
   has_and_belongs_to_many :users
   accepts_nested_attributes_for :users
   validates_presence_of :name
-  #before_save :check_active
   
   scope :all_active, ->{where(is_active: true).asc(:name)}
 
   def self.get_all_sorted_by_name
     Project.all.asc(:name)
-  end
-
-  def check_active
-    self.is_active = (self.is_active == 'true' ? true : false)
-    true
   end
 end
