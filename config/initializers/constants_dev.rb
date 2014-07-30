39738eb8293f33c54a743571a2261f124b4197c5
anuja <anuja@joshsoftware.com>
2014-07-30 20:10:31 +0530

    Removed unnecessary configuration

--- config/initializers/constants_dev.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env!= 'production'
-  GOOGLE_APP_ID = "1071290624779-83kdqqic2n53ejdpapg78t32dl1v8l0g.apps.googleusercontent.com"
-  GOOGLE_APP_SECRET = "7G9YaB6K3Ec4r9hnPpCOmVnw"
-  #GOOGLE_APP_ID = "329232665779.apps.googleusercontent.com"
-  #GOOGLE_APP_SECRET = "n2p9Gaf4ttIvmWWQGlvEq8d1"
-end
-
-GOOGLE_APT_REDIRECT_URL= "http://intranet.joshsoftware.com/google/auth_api"
