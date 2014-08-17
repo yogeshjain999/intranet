7d87fabaaaddbc39b172bdc3c943702c3b135f67
yogesh <yogesh@joshsoftware.com>
2014-08-17 13:31:48 +0530

    Feature : adding bonusly messages in user's show page

--- lib/data_api/api.rb
+++ lib/data_api/api.rb
@@ -1,49 +1,27 @@
 require 'net/http'
+
 module Api
   class Bonusly
 
-    def initialize(options = {})
-      options = {
-        size: 100
-      }.merge(options)
+    def initialize(token)
       @uri = URI.parse('https://bonus.ly/api/v1/bonuses')
-      @token = BONUSLY_TOKEN
-      get_request
+      @token = token
+      @http = Net::HTTP.new(@uri.host, @uri.port)
+      @http.use_ssl = true
+      @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     end
 
-    def get_request
-      http = Net::HTTP.new(@uri.host, @uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      response = http.get(@uri.path+"?access_token=#{@token}")
-      begin 
-        @data = JSON.parse(response.body)['result']
+    def bonusly_messages(options = {})
+      params = "access_token=#{@token}"
+      options.each_pair { |key, value| params += "&#{key}=#{value}" }
+      response = @http.get(@uri.path + "?" + params)
+      
+      begin
+        body = JSON.parse(response.body)
       rescue
-        @data = []
+        body = { 'success' => false, 'message' => 'User not listed in Bonusly' }
       end
-    end
-
-    def all_bonusly_messages
-      messages = []
-      @data.each { |d| messages << self.bonusly_message_for(d) }
-      messages
-    end
-
-    def bonusly_message_for(m, options = {})
-      "<li class='media'>
-        <a class='pull-left' href='#'>
-          <img class='media-object' src='#{m["receiver"]["profile_pic_url"]}' alt='#{m["receiver"]["short_name"]}' /> 
-        </a>
-        <a class='pull-right' href='#'>
-          <img class='media-object' src='#{m["giver"]["profile_pic_url"]}' alt='#{m["giver"]["short_name"]}' /> 
-        </a>
-        <div class='media-body'>
-          <h5 class='media-heading'>#{m["receiver"]["short_name"]} received <span class='label label-info'>₹#{m["amount"]}</span> from #{m["giver"]["short_name"]} <span class='label label-warning'> #{m["value"]} </span></h5>
-          <p>
-            #{m["reason"]} 
-          </p>
-        </div>
-      </li>"
+      body['success'] ? body['result'] : body['message']
     end
   end
 end
